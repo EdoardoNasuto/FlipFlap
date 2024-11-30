7ea14d4062e652d01c144f3dd82c8e54740ad64c
Edoardo Nasuto <edoardo.nasuto@gmail.com>
2024-11-30 18:40:50 +0100

    refactor: simplify game rules

--- src/rules/game_mode_rules.py
+++ src/rules/game_mode_rules.py
@@ -3,35 +3,32 @@ from src.rules.game_rules import *
 
 
 def base_game(num_rows, num_columns, num_obstacles, num_balls, size):
-    game = GameRules(num_rows, num_columns, num_obstacles, num_balls, size,
-                     obstacle_setup=ItemSetup.RANDOM, ball_setup=ItemSetup.RANDOM, item_type=ItemnType.BALLS,
-                     obstacle_color={"red": 0.34,
-                                     "blue": 0.33, "green": 0.33},
-                     ball_out_of_the_board=BallOutOfTheBoard.DELETE)
-    game.run_game()
+    GameRules(num_rows, num_columns, num_obstacles, num_balls, size,
+              obstacle_setup=ItemSetup.RANDOM, ball_setup=ItemSetup.RANDOM, item_type=ItemnType.BALLS,
+              obstacle_color={"red": 0.34,
+                              "blue": 0.33, "green": 0.33},
+              ball_out_of_the_board=BallOutOfTheBoard.DELETE)
 
 
 def trap_game(num_rows, num_columns, num_obstacles, num_balls, size):
-    game = GameRules(num_rows, num_columns, num_obstacles, num_balls, size,
-                     obstacle_setup=ItemSetup.EQUALLY, ball_setup=ItemSetup.RANDOM, item_type=ItemnType.BALLS,
-                     obstacle_color={"red": 0.40, "blue": 0.30,
-                                     "green": 0.20, "white": 0.10},
-                     obstacle_in_game_color_change={"red": 0.40, "blue": 0.30,
-                                                    "green": 0.30},
-                     ball_out_of_the_board=BallOutOfTheBoard.TRAVERSE_BOARD,
-                     change_obstacle_color_if_ball_present=ChangeObstacleColor.RANDOM,
-                     change_obstacle_color_on_click=ChangeObstacleColor.RANDOM)
-    game.run_game()
+    GameRules(num_rows, num_columns, num_obstacles, num_balls, size,
+              obstacle_setup=ItemSetup.EQUALLY, ball_setup=ItemSetup.RANDOM, item_type=ItemnType.BALLS,
+              obstacle_color={"red": 0.40, "blue": 0.30,
+                              "green": 0.20, "white": 0.10},
+              obstacle_in_game_color_change={"red": 0.40, "blue": 0.30,
+                                             "green": 0.30},
+              ball_out_of_the_board=BallOutOfTheBoard.TRAVERSE_BOARD,
+              change_obstacle_color_if_ball_present=ChangeObstacleColor.RANDOM,
+              change_obstacle_color_on_click=ChangeObstacleColor.RANDOM)
 
 
 def poule_renard_vipere_game(num_rows, num_columns, num_obstacles, num_balls, size):
-    game = GameRules(num_rows, num_columns, num_obstacles, num_balls, size,
-                     obstacle_setup=ItemSetup.RANDOM, ball_setup=ItemSetup.EQUALLY, item_type=ItemnType.ANIMALS,
-                     obstacle_color={"red": 0.30, "blue": 0.30,
-                                     "green": 0.30, "purple": 0.10},
-                     obstacle_in_game_color_change={
-                         "red": 0.34, "blue": 0.33, "green": 0.33},
-                     ball_out_of_the_board=BallOutOfTheBoard.TRAVERSE_BOARD,
-                     change_obstacle_color_if_ball_present=ChangeObstacleColor.RANDOM,
-                     ball_collision=BallCollision.SIMULATE_FOOD_CHAIN)
-    game.run_game()
+    GameRules(num_rows, num_columns, num_obstacles, num_balls, size,
+              obstacle_setup=ItemSetup.RANDOM, ball_setup=ItemSetup.EQUALLY, item_type=ItemnType.ANIMALS,
+              obstacle_color={"red": 0.30, "blue": 0.30,
+                              "green": 0.30, "purple": 0.10},
+              obstacle_in_game_color_change={
+                  "red": 0.34, "blue": 0.33, "green": 0.33},
+              ball_out_of_the_board=BallOutOfTheBoard.TRAVERSE_BOARD,
+              change_obstacle_color_if_ball_present=ChangeObstacleColor.RANDOM,
+              ball_collision=BallCollision.SIMULATE_FOOD_CHAIN)
--- src/rules/game_rules.py
+++ src/rules/game_rules.py
@@ -10,65 +10,64 @@ from typing import Optional
 
 class GameRules ():
     def __init__(self, num_rows: int, num_columns: int, num_obstacles: int, num_balls: int, size: int,
-                 obstacle_setup: ItemSetup, ball_setup: ItemSetup, item_type: ItemnType, obstacle_color: dict,
+                 obstacle_setup: ItemSetup, ball_setup: ItemSetup, item_type: ItemnType,
+
+                 obstacle_color: dict,
                  obstacle_in_game_color_change: Optional[dict] = None,
+
                  ball_out_of_the_board: Optional[BallOutOfTheBoard] = None,
                  change_obstacle_color_on_click: Optional[ChangeObstacleColor] = None,
                  change_obstacle_color_if_ball_present: Optional[ChangeObstacleColor] = None,
-                 ball_collision: Optional[BallCollision] = None
+                 ball_collision: Optional[BallCollision] = None,
+
+                 player_choices:  list[Optional[PlayerChoices]] = None,
+                 player_change_obstacle_color: Optional[ChangeObstacleColor] = None
                  ):
 
-        self.obstacle_setup = obstacle_setup
-        self.ball_setup = ball_setup
-        self.item_type = item_type
-        self.obstacle_color = obstacle_color
-        self.obstacle_in_game_color_change = obstacle_in_game_color_change
+        def setup_game():
+            Obstacle.available_colors = obstacle_color
+            setup_controller.setup_model(
+                obstacle_setup, ball_setup, item_type)
+            setup_controller.setup_view()
 
-        self.ball_out_of_the_board = ball_out_of_the_board
-        self.change_obstacle_color_on_click = change_obstacle_color_on_click
-        self.change_obstacle_color_if_ball_present = change_obstacle_color_if_ball_present
-        self.ball_collision = ball_collision
+        def run_game():
 
-        self.setup_controller = SetupController(
+            game.view.refresh()
+
+            if obstacle_in_game_color_change != None:
+                game.model.obstacle_colors = obstacle_in_game_color_change
+
+            while game.model.balls:
+                sleep(1)
+                if player_choices:
+                    game.players_turn()
+
+                for ball in list(game.model.balls):
+
+                    if change_obstacle_color_if_ball_present:
+                        game.change_obstacle_color_if_ball_present(
+                            ball, change_obstacle_color_on_click)
+
+                    if not game.move_ball(ball):
+                        if ball_out_of_the_board == BallOutOfTheBoard.DELETE:
+                            game.remove_ball(ball)
+                        if ball_out_of_the_board == BallOutOfTheBoard.TRAVERSE_BOARD:
+                            game.ball_traverse_board(ball)
+
+                    if change_obstacle_color_on_click:
+                        game.change_obstacle_color_on_click(
+                            change_obstacle_color_on_click)
+
+                if ball_collision == BallCollision.SIMULATE_FOOD_CHAIN:
+                    game.simulate_food_chain()
+
+                game.view.refresh()
+            game.view.exit_game()
+
+        setup_controller = SetupController(
             num_rows, num_columns, num_obstacles, num_balls, size)
-        self.setup_game()
+        setup_game()
 
-    def setup_game(self):
-        Obstacle.available_colors = self.obstacle_color
-        self.setup_controller.setup_model(
-            self.obstacle_setup, self.ball_setup, self.item_type)
-        self.setup_controller.setup_view()
-
-    def run_game(self):
-        self.game = GameController(
-            self.setup_controller.model, self.setup_controller.view)
-
-        self.game.view.refresh()
-
-        if self.obstacle_in_game_color_change != None:
-            self.game.model.obstacle_colors = self.obstacle_in_game_color_change
-
-        while self.game.model.balls:
-            sleep(1)
-
-            for ball in list(self.game.model.balls):
-
-                if self.change_obstacle_color_if_ball_present:
-                    self.game.change_obstacle_color_if_ball_present(
-                        ball, self.change_obstacle_color_on_click)
-
-                if not self.game.move_ball(ball):
-                    if self.ball_out_of_the_board == BallOutOfTheBoard.DELETE:
-                        self.game.remove_ball(ball)
-                    if self.ball_out_of_the_board == BallOutOfTheBoard.TRAVERSE_BOARD:
-                        self.game.ball_traverse_board(ball)
-
-                if self.change_obstacle_color_on_click:
-                    self.game.change_obstacle_color_on_click(
-                        self.change_obstacle_color_on_click)
-
-            if self.ball_collision == BallCollision.SIMULATE_FOOD_CHAIN:
-                self.game.simulate_food_chain()
-
-            self.game.view.refresh()
-        self.game.view.exit_game()
+        game = GameController(
+            setup_controller.model, setup_controller.view)
+        run_game()
